2062b862785434f5910544ad50eaf1be57326f56
noorbawzer <133597621+noorbawzer@users.noreply.github.com>
2023-05-21 20:33:46 +0300

    Add files via upload

---
+++ encapsulation/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        public string FirstName
+            {
+                get
+                {
+                    return firstName;
+                }
+                set
+                {
+                    firstName = value;
+                }
+            }
+            public string LastName
+            {
+                get
+                {
+                    return lastName;
+                }
+                set
+                {
+                    lastName = value;
+                }
+            }
+            public string Email
+            {
+                get
+                {
+                    return email;
+                }
+                set
+                {
+                    if (value.Contains("@") && value.EndsWith(".com"))
+                    {
+                        email = value;
+                    }
+                    else
+                        Console.WriteLine("Wrong value");
+                }
+            }
+            public string GetFullName()
+            {
+                return firstName + " " + lastName;
+
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Name : {GetFullName()},Email :{email}");
+            }
+        }
+    }
---
+++ encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.FirstName = "Noor";
+            p.LastName = "Bawzer";
+            p.Email = "wqaiuy@.com";
+            p.Print();
+        }
+    }
+}
